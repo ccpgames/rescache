251305625039319e0dee3e28450a04f97819b13f
breynisson <bjorgvin.reynisson@gmail.com>
2015-03-11 22:54:07 +0000

    Disabled moving the SharedCache folder on Mac.

--- move.py
+++ move.py
@@ -5,19 +5,22 @@ from paths import set_shared_cache_folder
 
 
 def move_cache(src, dest):
-    if os.path.exists(dest):
-        if os.path.isfile(dest):
-            print "'%s' exists as a file - can't move SharedCache folder" % dest
+    if sys.platform != "darwin":
+        if os.path.exists(dest):
+            if os.path.isfile(dest):
+                print "'%s' exists as a file - can't move SharedCache folder" % dest
+                sys.exit(1)
+
+            print "'%s' already exists - can't move SharedCache folder" % dest
             sys.exit(1)
 
-        print "'%s' already exists - can't move SharedCache folder" % dest
-        sys.exit(1)
+        try:
+            shutil.move(src, dest)
+        except (IOError, OSError):
+            pass
 
-    try:
-        shutil.move(src, dest)
-    except (IOError, OSError):
-        pass
+        set_shared_cache_folder(dest)
 
-    set_shared_cache_folder(dest)
-
-    print "SharedCache location has been moved to '%s'" % dest
+        print "SharedCache location has been moved to '%s'" % dest
+    else:
+        print "The SharedCache location cannot be moved on Mac"
